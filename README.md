486237210dd45da0b596d78c564654d9718533cb
Evgeniya <unis.tayn@gmail.com>
2018-02-01 20:43:06 +0300

    Installing

---
+++ README.md
@@ -0,0 +1,16 @@
+# ANETWORK
+
+## Installing
+
+In order to install the connected modules, type in your project:
+```
+npm install
+```
+
+## Starting
+
+to start the server, type:
+```
+npm run dev
+```
+
---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+
+gulp.task('styles', function() {
+  gulp.src('./styles/scss/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./styles/css/'));
+});
+
+//Watch task
+gulp.task('watch', function() {
+  gulp.watch('./styles/scss/*.scss',['styles']);
+});
+
+gulp.task('default', ['watch', 'styles']);
