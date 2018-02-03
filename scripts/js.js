070ece926541acb404627abcbc2019f4960cb205
Evgeniya <unis.tayn@gmail.com>
2018-02-03 21:14:21 +0300

    my layout of scroll menu

--- scripts/js.js
+++ scripts/js.js
@@ -1,16 +1,16 @@
 $(document).ready(function () {
   $('#fullpage').fullpage(
     {
+      anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage', 'fifthPage', 'sixthPage', 'seventhPage', 'eighthPage', 'ninthPage'],
+      menu: '#scrollMenu'
       // lockAnchors: false,
-      slidesNavigation: true,
-      navigation: true,
-      navigationPosition: 'left',
+      // slidesNavigation: true,
+      // navigation: true,
+      // navigationPosition: 'left',
       // showActiveTooltip: true,
       // menu: '#scroller-menu',
       // anchors:['slide1', 'slide2', 'slide3', 'slide4'],
-
       // navigationTooltips: ['firstSlide', 'secondSlide'],
-
       // slidesNavPosition: 'bottom'
     }
   );
