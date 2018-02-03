5063877b1902e40e768ffa35f86f48c95d9afba6
Evgeniya <unis.tayn@gmail.com>
2018-02-03 15:41:13 +0300

    laout of scroll menu

--- scripts/js.js
+++ scripts/js.js
@@ -1,4 +1,17 @@
+$(document).ready(function () {
+  $('#fullpage').fullpage(
+    {
+      // lockAnchors: false,
+      slidesNavigation: true,
+      navigation: true,
+      navigationPosition: 'left',
+      // showActiveTooltip: true,
+      // menu: '#scroller-menu',
+      // anchors:['slide1', 'slide2', 'slide3', 'slide4'],
 
-$(document).ready(function() {
-    $('#fullpage').fullpage();
+      // navigationTooltips: ['firstSlide', 'secondSlide'],
+
+      // slidesNavPosition: 'bottom'
+    }
+  );
 });
