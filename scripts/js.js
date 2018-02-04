44a1cf43c015944eeecff4c98f1a60850ae006ff
Evgeniya <unis.tayn@gmail.com>
2018-02-05 01:25:08 +0300

    shifted center phone

--- scripts/js.js
+++ scripts/js.js
@@ -24,11 +24,13 @@ $(document).ready(function () {
     $('.home-page__phone-left').addClass('home-page__phone-left--shifted');
     $('.home-page__watch-right').addClass('home-page__watch-right--down');
     $('.home-page__text').addClass('home-page__text--hidden');
+    $(this).addClass('home-page__phone-center--shifted');
   });
   $('.header__home').click(function () {
     $('.home-page__phone-left').removeClass('home-page__phone-left--shifted');
     $('.home-page__watch-right').removeClass('home-page__watch-right--down');
     $('.home-page__text').removeClass('home-page__text--hidden');
+    $('.home-page__phone-center').removeClass('home-page__phone-center--shifted');
     $.fn.fullpage.moveTo(1);
   })
 });
