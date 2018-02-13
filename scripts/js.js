b350e14633e7eedcb039f7bd57072b05394ce195
Evgeniya <unis.tyan@gmail.com>
2018-02-13 18:08:12 +0300

    added opening menu for Android and IPhone

--- scripts/js.js
+++ scripts/js.js
@@ -1,139 +1,160 @@
 $(document).ready(function () {
-  var menuOpened = false;
-  $('#fullpage').fullpage(
-    {
-      anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage', 'fifthPage', 'sixthPage', 'seventhPage', 'eighthPage', 'ninthPage'],
-      menu: '#scrollMenu',
-      onLeave: function (index, nextIndex) {
-        var scrollMenuElement = $('.scroll-menu');
-        if (nextIndex !== 1) {
-          scrollMenuElement.addClass('vision')
-        } else {
-          scrollMenuElement.removeClass('vision')
-        }
-        if (nextIndex === 2) {
-          $('.ip-telephony__phone-front').addClass('ip-telephony__phone-front--shifted');
-        } else {
-          $('.ip-telephony__phone-front').removeClass('ip-telephony__phone-front--shifted');
-        }
+    var menuOpened = false;
+    $('#fullpage').fullpage(
+        {
+            anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage', 'fifthPage', 'sixthPage', 'seventhPage', 'eighthPage', 'ninthPage'],
+            menu: '#scrollMenu',
+            onLeave: function (index, nextIndex) {
+                var scrollMenuElement = $('.scroll-menu');
+                if (nextIndex !== 1) {
+                    scrollMenuElement.addClass('vision')
+                } else {
+                    scrollMenuElement.removeClass('vision')
+                }
+                if (nextIndex === 2) {
+                    $('.ip-telephony__phone-front').addClass('ip-telephony__phone-front--shifted');
+                } else {
+                    $('.ip-telephony__phone-front').removeClass('ip-telephony__phone-front--shifted');
+                }
 
-        if (nextIndex === 3) {
-          $('.balance__phone-front').addClass('balance__phone-front--shifted');
-        } else {
-          $('.balance__phone-front').removeClass('balance__phone-front--shifted');
-        }
+                if (nextIndex === 3) {
+                    $('.balance__phone-front').addClass('balance__phone-front--shifted');
+                } else {
+                    $('.balance__phone-front').removeClass('balance__phone-front--shifted');
+                }
 
-        if (nextIndex === 4) {
-          $('.notification__phone-front').addClass('notification__phone-front--shifted');
-        } else {
-          $('.notification__phone-front').removeClass('notification__phone-front--shifted');
-        }
+                if (nextIndex === 4) {
+                    $('.notification__phone-front').addClass('notification__phone-front--shifted');
+                } else {
+                    $('.notification__phone-front').removeClass('notification__phone-front--shifted');
+                }
 
-        if (nextIndex === 5) {
-          $('.profile__phone-front').addClass('profile__phone-front--shifted');
-        } else {
-          $('.profile__phone-front').removeClass('profile__phone-front--shifted');
-        }
+                if (nextIndex === 5) {
+                    $('.profile__phone-front').addClass('profile__phone-front--shifted');
+                } else {
+                    $('.profile__phone-front').removeClass('profile__phone-front--shifted');
+                }
 
-        if (nextIndex === 6) {
-          $('.personal-account__phone-front').addClass('personal-account__phone-front--shifted');
-        } else {
-          $('.personal-account__phone-front').removeClass('personal-account__phone-front--shifted');
-        }
+                if (nextIndex === 6) {
+                    $('.personal-account__phone-front').addClass('personal-account__phone-front--shifted');
+                } else {
+                    $('.personal-account__phone-front').removeClass('personal-account__phone-front--shifted');
+                }
 
-        if (nextIndex === 7) {
-          $('.information__phone-front').addClass('information__phone-front--shifted');
-        } else {
-          $('.information__phone-front').removeClass('information__phone-front--shifted');
-        }
+                if (nextIndex === 7) {
+                    $('.information__phone-front').addClass('information__phone-front--shifted');
+                } else {
+                    $('.information__phone-front').removeClass('information__phone-front--shifted');
+                }
 
-        if ( index === 1  && !menuOpened) {
-          openMenu();
-          scrollMenuElement.removeClass('vision');
-          return false;
+                if (index === 1 && !menuOpened) {
+                    openMenu();
+                    scrollMenuElement.removeClass('vision');
+                    return false;
+                }
+            }
         }
-      }
+    );
+
+    $('.home-page__center-phone').hover(function (e) {
+        $(this).addClass('home-page__center-phone--hovered');
+        // $('.home-page__watch-right').addClass('home-page__watch-right--hover-center-phone-shifted');
+        // $('.home-page__phone-left').addClass('home-page__phone-left--hover-center-phone-shifted')
+    }, function (e) {
+        $(this).removeClass('home-page__center-phone--hovered');
+        // $('.home-page__watch-right').removeClass('home-page__watch-right--hover-center-phone-shifted');
+        // $('.home-page__phone-left').removeClass('home-page__phone-left--hover-center-phone-shifted')
+    });
+
+    $('.home-page__left-phone').hover(function () {
+        $(this).addClass('home-page__left-phone--hovered');
+        // $('.home-page__watch-right').addClass('home-page__watch-right--hover-center-phone-shifted');
+    }, function () {
+        $(this).removeClass('home-page__left-phone--hovered');
+        // $('.home-page__watch-right').removeClass('home-page__watch-right--hover-center-phone-shifted')
+    });
+
+    $('.home-page__center-phone').click(openAndroidMenu);
+
+    $('.home-page__left-phone').click(openIPhoneMenu);
+
+    $('.header__home').click(closeMenu);
+
+    $('.home-page__menu-elements-call').click(function () {
+        $.fn.fullpage.moveTo(2);
+    });
+
+    $('.home-page__menu-elements-balance').click(function () {
+        $.fn.fullpage.moveTo(3);
+    });
+
+    $('.home-page__menu-elements-bell').click(function () {
+        $.fn.fullpage.moveTo(4);
+    });
+
+    $('.home-page__menu-elements-profile').click(function () {
+        $.fn.fullpage.moveTo(5);
+    });
+
+    $('.home-page_menu-elements-info').click(function () {
+        $.fn.fullpage.moveTo(7);
+    });
+
+    function openAndroidMenu() {
+        if (menuOpened) {
+            return;
+        }
+        $('.home-page__text').addClass('home-page__text--hidden');
+        $('.home-page__menu-elements-balance').addClass('home-page__menu-elements-balance--shifted');
+        $('.home-page__menu-elements-support').addClass('home-page__menu-elements-support--shifted');
+        $('.home-page__menu-elements-profile').addClass('home-page__menu-elements-profile--shifted');
+        $('.home-page__menu-elements-info').addClass('home-page__menu-elements-info--shifted');
+        $('.home-page__menu-elements-bell').addClass('home-page__menu-elements-bell--shifted');
+        $('.home-page__menu-elements-call').addClass('home-page__menu-elements-call--shifted');
+        $('.home-page__right-text').addClass('home-page__right-text--shifted');
+        $('.home-page__left-text').addClass('home-page__left-text--shifted');
+        $('.home-page__left-phone').addClass('home-page__left-phone--shifted');
+        $('.home-page__watch-right').addClass('home-page__watch-right--down');
+        $('.home-page__center-phone').addClass('home-page__center-phone--shifted-center');
+        menuOpened = true;
     }
-  );
 
-  $('.home-page__center-phone').hover(function (e) {
-    $(this).addClass('home-page__center-phone--hovered');
-    // $('.home-page__watch-right').addClass('home-page__watch-right--hover-center-phone-shifted');
-      // $('.home-page__phone-left').addClass('home-page__phone-left--hover-center-phone-shifted')
-  }, function (e) {
-    $(this).removeClass('home-page__center-phone--hovered');
-    // $('.home-page__watch-right').removeClass('home-page__watch-right--hover-center-phone-shifted');
-    // $('.home-page__phone-left').removeClass('home-page__phone-left--hover-center-phone-shifted')
-  });
-
-  // $('.home-page__phone-left').hover(function () {
-  //     $('.home-page__phone-center').addClass('home-page__phone-center--hover-left-phone-shifted');
-  //     // $('.home-page__watch-right').addClass('home-page__watch-right--hover-center-phone-shifted');
-  // }, function () {
-  //     $('.home-page__phone-center').removeClass('home-page__phone-center--hover-left-phone-shifted');
-  //     // $('.home-page__watch-right').removeClass('home-page__watch-right--hover-center-phone-shifted')
-  // });
-
-  $('.home-page__active-phone').click(openMenu);
-
-  $('.header__home').click(closeMenu);
-
-  $('.home-page__menu-elements-call').click(function () {
-    $.fn.fullpage.moveTo(2);
-  });
-
-  $('.home-page__menu-elements-balance').click(function () {
-    $.fn.fullpage.moveTo(3);
-  });
-
-  $('.home-page__menu-elements-bell').click(function () {
-    $.fn.fullpage.moveTo(4);
-  });
-
-  $('.home-page__menu-elements-profile').click(function () {
-    $.fn.fullpage.moveTo(5);
-  });
-
-  $('.home-page__menu-elements-info').click(function () {
-    $.fn.fullpage.moveTo(7);
-  });
-
-  function openMenu() {
-    if (menuOpened) {
-      return;
+    function closeMenu() {
+        $('.home-page__text').removeClass('home-page__text--hidden');
+        $('.home-page__menu-elements-balance').removeClass('home-page__menu-elements-balance--shifted');
+        $('.home-page__menu-elements-support').removeClass('home-page__menu-elements-support--shifted');
+        $('.home-page__menu-elements-profile').removeClass('home-page__menu-elements-profile--shifted');
+        $('.home-page__menu-elements-info').removeClass('home-page__menu-elements-info--shifted');
+        $('.home-page__menu-elements-bell').removeClass('home-page__menu-elements-bell--shifted');
+        $('.home-page__menu-elements-call').removeClass('home-page__menu-elements-call--shifted');
+        $('.home-page__right-text').removeClass('home-page__right-text--shifted');
+        $('.home-page__left-text').removeClass('home-page__left-text--shifted');
+        $('.home-page__left-phone').removeClass('home-page__left-phone--shifted');
+        $('.home-page__left-phone').removeClass('home-page__left-phone--shifted-center');
+        $('.home-page__watch-right').removeClass('home-page__watch-right--down');
+        $('.home-page__center-phone').removeClass('home-page__center-phone--shifted-center');
+        $('.home-page__center-phone').removeClass('home-page__center-phone--shifted');
+        $.fn.fullpage.moveTo(1);
+        menuOpened = false;
     }
-    $('.home-page__left-phone').addClass('home-page__left-phone--shifted');
-    $('.home-page__watch-right').addClass('home-page__watch-right--down');
-    $('.home-page__text').addClass('home-page__text--hidden');
-    $('.home-page__phone-center').addClass('home-page__phone-center--shifted');
-    $('.home-page__menu-elements-balance').addClass('home-page__menu-elements-balance--shifted');
-    $('.home-page__menu-elements-support').addClass('home-page__menu-elements-support--shifted');
-    $('.home-page__menu-elements-profile').addClass('home-page__menu-elements-profile--shifted');
-    $('.home-page__menu-elements-info').addClass('home-page__menu-elements-info--shifted');
-    $('.home-page__menu-elements-bell').addClass('home-page__menu-elements-bell--shifted');
-    $('.home-page__menu-elements-call').addClass('home-page__menu-elements-call--shifted');
-    $('.home-page__right-text').addClass('home-page__right-text--shifted');
-    $('.home-page__left-text').addClass('home-page__left-text--shifted');
-    $('.home-page__active-phone').addClass('home-page__active-phone--shifted');
-    menuOpened = true;
-  }
 
-  function closeMenu() {
-    $('.home-page__left-phone').removeClass('home-page__left-phone--shifted');
-    $('.home-page__watch-right').removeClass('home-page__watch-right--down');
-    $('.home-page__text').rmoveCleass('home-page__text--hidden');
-    $('.home-page__phone-center').removeClass('home-page__phone-center--shifted');
-    $('.home-page__menu-elements-balance').removeClass('home-page__menu-elements-balance--shifted');
-    $('.home-page__menu-elements-support').removeClass('home-page__menu-elements-support--shifted');
-    $('.home-page__menu-elements-profile').removeClass('home-page__menu-elements-profile--shifted');
-    $('.home-page__menu-elements-info').removeClass('home-page__menu-elements-info--shifted');
-    $('.home-page__menu-elements-bell').removeClass('home-page__menu-elements-bell--shifted');
-    $('.home-page__menu-elements-call').removeClass('home-page__menu-elements-call--shifted');
-    $('.home-page__right-text').removeClass('home-page__right-text--shifted');
-    $('.home-page__left-text').removeClass('home-page__left-text--shifted');
-    $('.home-page__active-phone').removeClass('home-page__active-phone--shifted');
-    $.fn.fullpage.moveTo(1);
-    menuOpened = false;
-  }
+    function openIPhoneMenu() {
+        if (menuOpened) {
+            return;
+        }
+        $('.home-page__text').addClass('home-page__text--hidden');
+        $('.home-page__menu-elements-balance').addClass('home-page__menu-elements-balance--shifted');
+        $('.home-page__menu-elements-support').addClass('home-page__menu-elements-support--shifted');
+        $('.home-page__menu-elements-profile').addClass('home-page__menu-elements-profile--shifted');
+        $('.home-page__menu-elements-info').addClass('home-page__menu-elements-info--shifted');
+        $('.home-page__menu-elements-bell').addClass('home-page__menu-elements-bell--shifted');
+        $('.home-page__menu-elements-call').addClass('home-page__menu-elements-call--shifted');
+        $('.home-page__right-text').addClass('home-page__right-text--shifted');
+        $('.home-page__left-text').addClass('home-page__left-text--shifted');
+        $('.home-page__left-phone').addClass('home-page__left-phone--shifted-center');
+        $('.home-page__watch-right').addClass('home-page__watch-right--down');
+        $('.home-page__center-phone').addClass('home-page__center-phone--shifted');
+        menuOpened = true;
+    }
 
 });
